f002d41128d2614b3afabea566f5cb9f0c008789
KonstantinKolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2024-05-23 08:45:49 +0300

    Добавил комментарии к коду, для лучшего понимания, что этот код делает

--- 8/ultro_Pro.py
+++ 8/ultro_Pro.py
@@ -1,21 +1,28 @@
-"""Напишите функцию, которая находит две точки: где наклон графика функции самый крутой (в любую сторону), где наклон самый пологий, и отображает на графике функции эти точки зелёным и красным кругом соответственно.
 """
-
-
-"""
-Задача 2
-Напишите функцию, которая находит все экстремумы двумерной функции z = sin(x) * sin(y) в диапазоне [x = -3.14, x = 3.14], [y = -3.14, y = 3.14], а также:
-
-a) Возвращает листы с координатами двумерных точек - 4 листа - локальный и глобальный минимум и максимум.
-
-b) Выводит график такой функции.
+Задача 1
+Напишите функцию, которая находит две точки:
+где наклон графика функции самый крутой (в любую сторону),
+где наклон самый пологий, и отображает на графике функции эти точки зелёным
+и красным кругом соответственно.
 """
 
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+
+
 def find_extremums(func, x_range, y_range):
+    """
+    Функция для нахождения локальных и глобальных экстремумов двумерной функции.
+
+    Аргументы:
+    func - функция, для которой ищутся экстремумы
+    x_range - диапазон значений x
+    y_range - диапазон значений y
+
+    Возвращает:
+    словарь с локальными и глобальными минимумами и максимумами
+    """
     x = np.linspace(x_range[0], x_range[1], 100)
     y = np.linspace(y_range[0], y_range[1], 100)
     X, Y = np.meshgrid(x, y)
@@ -29,13 +36,13 @@ def find_extremums(func, x_range, y_range):
     # Находим локальные минимумы и максимумы
     for i in range(1, len(x) - 1):
         for j in range(1, len(y) - 1):
-            if (i > 0 and i < len(x) - 1 and j > 0 and j < len(y) - 1 and
-                Z[i, j] < Z[i - 1, j] and Z[i, j] < Z[i + 1, j] and
-                Z[i, j] < Z[i, j - 1] and Z[i, j] < Z[i, j + 1]):
+            # Проверяем, является ли точка локальным минимумом
+            if (Z[i, j] < Z[i - 1, j] and Z[i, j] < Z[i + 1, j] and
+                    Z[i, j] < Z[i, j - 1] and Z[i, j] < Z[i, j + 1]):
                 extremums['min']['local'].append((x[i], y[j], Z[i, j]))
-            if (i > 0 and i < len(x) - 1 and j > 0 and j < len(y) - 1 and
-                Z[i, j] > Z[i - 1, j] and Z[i, j] > Z[i + 1, j] and
-                Z[i, j] > Z[i, j - 1] and Z[i, j] > Z[i, j + 1]):
+            # Проверяем, является ли точка локальным максимумом
+            if (Z[i, j] > Z[i - 1, j] and Z[i, j] > Z[i + 1, j] and
+                    Z[i, j] > Z[i, j - 1] and Z[i, j] > Z[i, j + 1]):
                 extremums['max']['local'].append((x[i], y[j], Z[i, j]))
 
     # Находим глобальные минимумы и максимумы
@@ -44,7 +51,17 @@ def find_extremums(func, x_range, y_range):
 
     return extremums
 
+
 def plot_function(func, x_range, y_range, extremums):
+    """
+    Функция для визуализации двумерной функции с отмеченными экстремумами.
+
+    Аргументы:
+    func - функция, для которой ищутся экстремумы
+    x_range - диапазон значений x
+    y_range - диапазон значений y
+    extremums - словарь с экстремумами
+    """
     x = np.linspace(x_range[0], x_range[1], 100)
     y = np.linspace(y_range[0], y_range[1], 100)
     X, Y = np.meshgrid(x, y)
@@ -54,6 +71,7 @@ def plot_function(func, x_range, y_range, extremums):
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(X, Y, Z, cmap='viridis')
 
+    # Отмечаем локальные и глобальные экстремумы на графике
     for extremum in extremums['min']['local']:
         ax.scatter(extremum[0], extremum[1], extremum[2], color='green')
     for extremum in extremums['max']['local']:
@@ -65,9 +83,22 @@ def plot_function(func, x_range, y_range, extremums):
 
     plt.show()
 
+
+"""
+Задача 2
+Напишите функцию, которая находит все экстремумы двумерной функции z = sin(x) * sin(y)
+в диапазоне [x = -3.14, x = 3.14], [y = -3.14, y = 3.14], а также:
+
+a) Возвращает листы с координатами двумерных точек - 4 листа - локальный и глобальный минимум и максимум.
+
+b) Выводит график такой функции.
+"""
+
+
 # Функция, для которой ищем экстремумы
 def func(x, y):
     return np.sin(x) * np.sin(y)
+
 
 # Находим экстремумы
 extremums = find_extremums(func, [-3.14, 3.14], [-3.14, 3.14])
